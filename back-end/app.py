72f592e04e8bd51e35efc2ba43506d4c7fc7de5c
Nirav Patel <npp43@njit.edu>
2020-09-27 01:21:56 -0400

    Adding files

---
+++ back-end/app.py
@@ -0,0 +1,14 @@
+import pika
+import time
+
+time.sleep(10)
+
+print("Back End is Running Now!")
+
+#credentials = pika.PlainCredentials('guest', 'guest')
+#connection = pika.BlockingConnection(
+#       pika.ConnectionParameters(
+#           host='messaging',
+#           credentials=credentials
+#        )
+#)
