ae80a991894284799e47ce307d663fde02db7e52
Adit Patel <asp234@njit.edu>
2020-10-18 22:32:02 -0400

    milestone

---
+++ db/user.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IT490;
+
+CREATE TABLE users
+(
+    username varchar(50),
+    firstname varchar(50),
+    lastname varchar(50) not null,
+    email varchar(255) not null unique,
+    hash VARCHAR(255) NOT NULL
+);
