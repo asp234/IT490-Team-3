276ae6d2fc0fe4e02bb3057f701e1e8c85a305a7
Darshin Patel <dmp73@njit.edu>
2020-10-27 04:23:45 -0700

    Authentication process completed working on design

--- frontend/app.py
+++ frontend/app.py
@@ -1,60 +1,80 @@
-from flask import Flask, render_template, request, session, redirect, flash
-from werkzeug.security import check_password_hash, generate_password_hash
-import pika
-import messaging
-import os
+"""Flask Login Example and instagram fallowing find"""
+
+from flask import Flask, url_for, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+from instagram import getfollowedby, getname
+
 
 app = Flask(__name__)
-app.secret_key = os.environ['FLASK_SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/login', methods=['GET','POST'])
+
+class User(db.Model):
+    """ Create user table"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    """ Session control"""
+    if not session.get('logged_in'):
+        return render_template('index.html')
+    else:
+        if request.method == 'POST':
+            username = getname(request.form['username'])
+            return render_template('index.html', data=getfollowedby(username))
+        return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        msg = messaging.Messaging()
-        msg.send('GETHASH', { 'username': username })
-        response = msg.receive()
-        if response['success'] != True:
-            return "Login failed."
-        if check_password_hash(response['hash'], password):
-            session['username'] = username
-            return redirect('/userpage')
-        else:
-            return "Login failed."
-    return render_template('login.html')
-
-
-@app.route('/register', methods=['GET','POST'])
-def register():
-    if request.method == 'POST':
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        email = request.form['email']
-        username = request.form['username']
-        password = request.form['password']
-        confirmpassword = request.form['confirmpassword']
-        if password == confirmpassword:
-            msg = messaging.Messaging()
-            msg.send(
-                'REGISTER',
-                {
-                    'firstname' : firstname,
-                    'lastname' : lastname,
-                    'email': email,
-                    'username' : username,
-                    'hash': generate_password_hash(password)
-                }
-            )
-            response = msg.receive()
-            if response['success']:
-                session['username'] = username
-                return redirect('/userpage')
+    """Login Form"""
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        name = request.form['username']
+        passw = request.form['password']
+        try:
+            data = User.query.filter_by(username=name, password=passw).first()
+            if data is not None:
+                session['logged_in'] = True
+                return redirect(url_for('home'))
             else:
-                return f"{response['message']}"
+                return 'Dont Login'
+        except:
+            return "Dont Login"
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    """Register Form"""
+    if request.method == 'POST':
+        new_user = User(
+            username=request.form['username'],
+            password=request.form['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        return render_template('login.html')
     return render_template('register.html')
 
-@app.route('/logout')
+
+@app.route("/logout")
 def logout():
-    session.pop('username', None)
-    return redirect('/')
+    """Logout Form"""
+    session['logged_in'] = False
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.debug = True
+    db.create_all()
+    app.secret_key = "123"
+    app.run(host='0.0.0.0')
+    
